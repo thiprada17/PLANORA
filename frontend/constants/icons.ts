74390959120c9a2d4bf995ca4e256aa3eb3997c4
thiprada17 <thiprada.mop@dome.tu.ac.th>
2026-01-25 14:09:43 +0700

    เพิ่ม constant

---
+++ frontend/constants/icons.ts
@@ -0,0 +1,12 @@
+import deadline from "@/assets/icons/assignment_late.png";
+import check from "@/assets/icons/check.png";
+import folder from "@/assets/icons/folder_open.png";
+import person_add from "@/assets/icons/group_add.png";
+
+
+export const icons = {
+  deadline,
+  check,
+  folder,
+  person_add
+};
---
+++ frontend/types.d.ts
@@ -0,0 +1,4 @@
+declare module "*.png" {
+  const content: import("react-native").ImageSourcePropType;
+  export default content;
+}
