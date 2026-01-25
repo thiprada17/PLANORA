b37995325942b866a5e6ea381ae1c1a4cdc802c7
thiprada17 <thiprada.mop@dome.tu.ac.th>
2026-01-25 21:48:17 +0700

    create post เสดประมาณนึง ฟ้อนใช้ไม่ได้

--- frontend/constants/icons.ts
+++ frontend/constants/icons.ts
@@ -2,11 +2,14 @@ import deadline from "@/assets/icons/assignment_late.png";
 import check from "@/assets/icons/check.png";
 import folder from "@/assets/icons/folder_open.png";
 import person_add from "@/assets/icons/group_add.png";
-
+import topic from "@/assets/icons/topic.png";
+import back from "@/assets/icons/back.png";
 
 export const icons = {
   deadline,
   check,
   folder,
-  person_add
+  person_add,
+  topic,
+  back
 };
