5e73b19fc2228ca74df9364292fe41d151bd5cb6
MissMieiei <muaymemon@gmail.com>
2026-01-29 12:03:40 +0700

    test

---
+++ frontend/app/project/[projectId]/board.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const board = () => {
+  return (
+    <View>
+      <Text>board</Text>
+    </View>
+  )
+}
+
+export default board
+
+const styles = StyleSheet.create({})
