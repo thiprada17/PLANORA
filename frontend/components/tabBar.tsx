1c4fddc9dfec75f719ec876f9e91ac3a37b33a0a
FALONYN <lookfai1339@gmail.com>
2026-02-07 22:24:17 +0700

    tabbar หน้า board จ้า

--- frontend/components/tabBar.tsx
+++ frontend/components/tabBar.tsx
@@ -1,121 +1,183 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
-  View,
-  Text,
-  Animated,
-  Pressable,
-  Dimensions,
-  Image,
+    View,
+    Text,
+    Animated,
+    Pressable,
+    Dimensions,
+    Image,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { icons } from "@/constants/icons";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 const TABBAR_WIDTH = width * 0.8;
 
 const Icon = ({
-  name,
-  size = 20,
+    name,
+    size = 20,
 }: {
-  name: keyof typeof icons;
-  size?: number;
+    name: keyof typeof icons;
+    size?: number;
 }) => (
-  <Image
-    source={icons[name]}
-    style={{ width: size, height: size }}
-    resizeMode="contain"
-  />
+    <Image
+        source={icons[name]}
+        style={{ width: size, height: size }}
+        resizeMode="contain"
+    />
 );
 
 type TabBarProps = {
-  visible: boolean;
-  onClose: () => void;
+    visible: boolean;
+    onClose: () => void;
 };
 
 export default function TabBar({ visible, onClose }: TabBarProps) {
-  const translateX = useRef(new Animated.Value(-TABBAR_WIDTH)).current;
+    const [user, setUser] = useState<{
+        name: string | null;
+        email: string | null;
+        profile: string | null;
+    }>({
+        name: null,
+        email: null,
+        profile: null,
+    });
+    useEffect(() => {
+        const loadUser = async () => {
+            const username = await AsyncStorage.getItem("username");
+            const email = await AsyncStorage.getItem("email");
+            const profile = await AsyncStorage.getItem("profile");
 
-  useEffect(() => {
-    Animated.timing(translateX, {
-      toValue: visible ? 0 : -TABBAR_WIDTH,
-      duration: 280,
-      useNativeDriver: true,
-    }).start();
-  }, [visible]);
+            setUser({
+                name: username,
+                email,
+                profile,
+            });
+        };
 
-  if (!visible) return null;
+        loadUser();
+    }, []);
 
-  return (
-    <View className="absolute inset-0 z-50">
-      <Pressable
-        className="absolute inset-0 bg-black/40"
-        onPress={onClose}
-      />
+    const translateX = useRef(new Animated.Value(-TABBAR_WIDTH)).current;
+    const [shouldRender, setShouldRender] = useState(visible);
 
-      {/* tab bar */}
-      <Animated.View
-        style={{
-          transform: [{ translateX }],
-          width: TABBAR_WIDTH,
-        }}
-        className="h-full bg-white rounded-r-3xl shadow-xl"
-      >
-        <SafeAreaView className="flex-1 px-6">
-          <View className="flex-row items-center justify-between mt-4 mb-8">
-            <Text className="text-[32px] font-bold">
-              Planora
-            </Text>
+    useEffect(() => {
+        if (visible) {
+            setShouldRender(true);
+        }
 
-            <Pressable onPress={onClose}>
-              <Icon name="menu" size={22} />
-            </Pressable>
-          </View>
+        Animated.timing(translateX, {
+            toValue: visible ? 0 : -TABBAR_WIDTH,
+            duration: 280,
+            useNativeDriver: true,
+        }).start(() => {
+            if (!visible) {
+                setShouldRender(false);
+            }
+        });
+    }, [visible]);
+    if (!shouldRender) return null;
 
-          {/* Menu */}
-          <View className="space-y-6">
-            <Pressable className="flex-row items-center space-x-3">
-              <Icon name="home_garden" size={20} />
-              <Text className="text-[18px] font-medium">
-                Pony's Homepage
-              </Text>
-            </Pressable>
-
-            {/* Project */}
-            <View>
-              <View className="flex-row items-center space-x-3 mb-4">
-                <Icon name="folder" size={20} />
-                <Text className="text-[18px] font-medium">
-                  Project
-                </Text>
-              </View>
-
-              <View className="ml-6 pl-4 border-l border-gray-300 space-y-4">
-                <TreeItem icon="dashboard" label="DashBoard" />
-                <TreeItem icon="board" label="Board" />
-                <TreeItem icon="chat" label="Chat" />
-                <TreeItem icon="settings" label="Setting" />
-              </View>
-            </View>
-          </View>
-        </SafeAreaView>
-      </Animated.View>
-    </View>
-  );
+    return (
+        <View className="absolute inset-0 z-50">
+            <Pressable
+                className="absolute inset-0 bg-GRAY/80"
+                onPress={onClose}
+            />
+            {/* tab bar */}
+            <Animated.View
+                style={{
+                    transform: [{ translateX }],
+                    width: TABBAR_WIDTH,
+                }}
+                className="h-full bg-white rounded-r-3xl shadow-xl">
+                <SafeAreaView className="flex-1 px-8">
+                    <View className="flex-1 ">
+                        <View className="flex-row items-center justify-between mb-8 pt-10">
+                            <Text className="text-[32px] font-kanitBold">
+                                Planora
+                            </Text>
+                            <Pressable onPress={onClose}>
+                                <Icon name="menu" size={22} />
+                            </Pressable>
+                        </View>
+                        {/* Menu */}
+                        <View className="space-y-6">
+                            <Pressable className="flex-row items-center space-x-3 mb-3">
+                                <Icon name="home_garden" size={23} />
+                                <Text className="text-[18px] font-kanitMedium ml-2">
+                                    {user.name ?? "Guest"}'s Homepage
+                                </Text>
+                            </Pressable>
+                            {/* Project */}
+                            <View>
+                                <View className="flex-row items-center space-x-3 mb-1">
+                                    <Icon name="folder" size={23} />
+                                    <Text className="text-[18px] font-kanitMedium ml-2">
+                                        Project
+                                    </Text>
+                                </View>
+                                <View className="ml-0 mt-1.5">
+                                    <TreeItem icon="dashboard" label="Dashboard" />
+                                    <TreeItem icon="board" label="Board" />
+                                    <TreeItem icon="chat" label="Chat" />
+                                    <TreeItem icon="settings" label="Setting" isLast />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View className="border-t border-gray-200 pt-4 pb-6">
+                        <View className="flex-row items-center">
+                            <Image
+                                source={user.profile ? { uri: user.profile } : icons.profile}
+                                className={
+                                    user.profile
+                                        ? "w-10 h-10 rounded-full mr-3"
+                                        : "w-6 h-6 mr-3"
+                                }
+                            />
+                            <View className="flex-1">
+                                <Text className="font-kanitMedium text-sm text-black">
+                                    {user.name ?? "Guest"}
+                                </Text>
+                                <Text className="font-kanitRegular text-xs text-neutral-500">
+                                    {user.email ?? ""}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </Animated.View>
+        </View>
+    );
 }
 
 function TreeItem({
-  icon,
-  label,
+    icon,
+    label,
+    isLast = false,
 }: {
-  icon: keyof typeof icons;
-  label: string;
+    icon: keyof typeof icons;
+    label: string;
+    isLast?: boolean;
 }) {
-  return (
-    <Pressable className="flex-row items-center space-x-3">
-      <Icon name={icon} size={18} />
-      <Text className="text-[17px]">
-        {label}
-      </Text>
-    </Pressable>
-  );
+    return (
+        <View className="flex-row h-10">
+            <View className="relative w-6 items-center">
+                <View className="absolute top-0 w-[2px] h-1/2 bg-GRAY" />
+                {/* อันล่างสุด */}
+                {!isLast && (
+                    <View className="absolute bottom-0 w-[2px] h-1/2 bg-GRAY" />
+                )}
+                <View className="absolute top-1/2 w-4 h-[2px] bg-GRAY left-1/2" />
+            </View>
+            <Pressable className="flex-row items-center space-x-3 ml-2">
+                <Icon name={icon} size={20} />
+                <Text className="text-[17px] font-kanitMedium pl-2">
+                    {label}
+                </Text>
+            </Pressable>
+        </View>
+    );
 }
